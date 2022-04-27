3e219d9763134f6685441ff1054da19a0c65687c
Oleg Kozyrev <okozyrev@ozon.ru>
2022-04-27 21:21:56 +0300

    add hello world

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	str := "Hello, OTUS!"
+
+	fmt.Println(stringutil.Reverse(str))
 }
