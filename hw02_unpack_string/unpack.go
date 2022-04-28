e5e6023ccc394f7532247b6c0a2e6c7528d7df58
Oleg Kozyrev <okozyrev@ozon.ru>
2022-04-28 18:40:26 +0300

    refactoring

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -12,61 +12,53 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(inStr string) (string, error) {
 	if len(inStr) == 0 {
-		return inStr, nil
+		return "", nil
 	}
-
-	var prevSymbol rune
-	var prevCh rune
 
 	var res strings.Builder
+	prevDigit := -1
+	var symbol rune
 	for _, ch := range inStr {
-		if unicode.IsDigit(ch) {
-			if unicode.IsDigit(prevCh) {
-				return "", ErrInvalidString
-			}
-			if prevCh == 0 || prevSymbol == 0 {
-				return "", ErrInvalidString
-			}
-		} else {
-			if unicode.IsDigit(prevCh) {
-				count, err := strconv.Atoi(string(prevCh))
-				if err != nil {
-					return "", ErrInvalidString
-				}
-
-				res.WriteString(strings.Repeat(string(prevSymbol), count))
-			} else if prevCh != 0 {
-				res.WriteRune(prevSymbol)
+		if !unicode.IsDigit(ch) {
+			if prevDigit >= 0 {
+				res.WriteString(strings.Repeat(string(symbol), prevDigit))
+			} else if symbol > 0 {
+				res.WriteRune(symbol)
 			}
 
-			prevSymbol = ch
+			symbol = ch
+			prevDigit = -1
+			continue
 		}
 
-		prevCh = ch
+		if prevDigit >= 0 || symbol == 0 {
+			return "", ErrInvalidString
+		}
+
+		var err error
+		prevDigit, err = strconv.Atoi(string(ch))
+		if err != nil {
+			return "", ErrInvalidString
+		}
 	}
 
-	end, err := getEndStr(inStr, prevCh, prevSymbol)
+	last, err := getEndStr(inStr, symbol, prevDigit)
 	if err != nil {
-		return "", err
+		return "", ErrInvalidString
 	}
 
-	res.WriteString(end)
+	res.WriteString(last)
 	return res.String(), nil
 }
 
-func getEndStr(inStr string, prevCh rune, prevSymbol rune) (string, error) {
+func getEndStr(inStr string, symbol rune, prevDigit int) (string, error) {
 	lastRune, _ := utf8.DecodeLastRuneInString(inStr)
 	if lastRune == utf8.RuneError {
 		return "", ErrInvalidString
 	}
 
 	if unicode.IsDigit(lastRune) {
-		count, err := strconv.Atoi(string(prevCh))
-		if err != nil {
-			return "", ErrInvalidString
-		}
-
-		return strings.Repeat(string(prevSymbol), count), nil
+		return strings.Repeat(string(symbol), prevDigit), nil
 	}
 
 	return string(lastRune), nil
