7dbf526179637daa7bb601a06ce2b51817060207
Oleg Kozyrev <okozyrev@ozon.ru>
2022-05-17 12:51:05 +0300

    add simple impl.

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,35 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	for _, st := range stages {
+		tmpChan := make(chan interface{})
+		go func(ch In) {
+			defer close(tmpChan)
+			for data := range ch {
+				tmpChan <- data
+			}
+		}(in)
+
+		in = st(tmpChan)
+	}
+
+	return in
 }
+
+//func runStage(ctx context.Context, in In, stage Stage) Out {
+//	out := stage(in)
+//
+//	go func() {
+//		//for data := range in {
+//		//
+//		//}
+//		select {
+//		case data := <-in:
+//			out <- stage(data)
+//		case <-ctx.Done():
+//			return
+//		}
+//	}()
+//
+//	return out
+//}
