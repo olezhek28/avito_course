0f480333bab3607b1069dfd88de98bab9097fdaa
Oleg Kozyrev <okozyrev@ozon.ru>
2022-06-21 16:37:38 +0300

    add env reader and tests for him

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+)
+
+var ErrInvalidFilename = errors.New("invalid filename")
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +25,61 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	fileInfos, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read dir: %w", err)
+	}
+
+	env := make(Environment, len(fileInfos))
+
+	for _, fInfo := range fileInfos {
+		if fInfo.IsDir() {
+			continue
+		}
+
+		if strings.Contains(fInfo.Name(), "=") {
+			return nil, ErrInvalidFilename
+		}
+
+		if fInfo.Size() == 0 {
+			delete(env, fInfo.Name())
+			continue
+		}
+
+		var val string
+		val, err = getValueFromFile(path.Join(dir, fInfo.Name()))
+		if err != nil {
+			return nil, err
+		}
+
+		env[fInfo.Name()] = EnvValue{
+			Value: val,
+		}
+	}
+
+	return env, nil
+}
+
+func getValueFromFile(fullName string) (string, error) {
+	f, err := os.Open(fullName)
+	if err != nil {
+		return "", fmt.Errorf("failed to open file: %w", err)
+	}
+	defer func() {
+		err = f.Close()
+		if err != nil {
+			fmt.Errorf("failed to close file: %w", err)
+		}
+	}()
+
+	buf := bufio.NewReader(f)
+	line, err := buf.ReadBytes('\n')
+	if err != nil && !errors.Is(err, io.EOF) {
+		return "", fmt.Errorf("failed to read file: %w", err)
+	}
+
+	val := bytes.ReplaceAll(line, []byte{0}, []byte{'\n'})
+	valStr := strings.TrimRight(string(val), " \t\n")
+
+	return valStr, nil
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,66 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("correct reading", func(t *testing.T) {
+		expected := Environment{
+			"BAR":   EnvValue{Value: "bar"},
+			"FOO":   EnvValue{Value: "   foo\nwith new line"},
+			"HELLO": EnvValue{Value: `"hello"`},
+			"EMPTY": EnvValue{Value: ""},
+		}
+		actual, err := ReadDir("testdata/env")
+
+		require.NoError(t, err)
+		require.Equal(t, expected, actual)
+	})
+
+	t.Run("not existing dir", func(t *testing.T) {
+		_, err := ReadDir("not_existing_dir")
+		require.Error(t, err)
+	})
+
+	t.Run("empty dir", func(t *testing.T) {
+		dir := "testdata/empty"
+
+		err := os.Mkdir(dir, os.FileMode(0755))
+		require.Nil(t, err)
+		defer func() {
+			err = os.Remove(dir)
+			require.NoError(t, err)
+		}()
+
+		env, err := ReadDir(dir)
+		require.NoError(t, err)
+		require.Equal(t, Environment{}, env)
+	})
+
+	t.Run("invalid filename", func(t *testing.T) {
+		dir := "testdata/invalid_env"
+
+		err := os.Mkdir(dir, os.FileMode(0755))
+		require.NoError(t, err)
+		defer func() {
+			err = os.RemoveAll(dir)
+			require.NoError(t, err)
+		}()
+
+		f, err := os.Create(path.Join(dir, "NAME=INVALID"))
+		require.Nil(t, err)
+		defer func() {
+			err = f.Close()
+			require.NoError(t, err)
+		}()
+
+		env, err := ReadDir(dir)
+		require.Zero(t, env)
+		require.Equal(t, ErrInvalidFilename, err)
+	})
 }
